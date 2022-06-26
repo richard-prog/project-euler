e3dab6200874f54a3e41359c513e8a7b2f81528e
Richard Reitz <richard.reitz@protonmail.com>
2022-06-26 18:58:32 -0400

    Add functionality to divisors for problem 21

--- src/divisors.rs
+++ src/divisors.rs
@@ -9,6 +9,33 @@ pub fn count_divisors(num: u32, primes: &Vec<u32>) -> u32 {
     num_divisors
 }
 
+pub fn get_proper_divisors(num: u64, primes: &Vec<u32>) -> Vec<u32> {
+    let factorization = primes::factor(num, primes);
+    let mut divisors: Vec<u32> = vec![1];
+    for (base, exp) in factorization {
+	extend_divisors(&mut divisors, base, exp);
+    }
+    divisors.pop();
+    divisors
+}
+
+pub fn sum_proper_divisors(num: u64) -> u32 {
+    get_proper_divisors(num, &primes::get_primes()).iter().sum()
+}
+
+fn extend_divisors(divisors: &mut Vec<u32>, base: u32, exponent: u32) {
+    let n = divisors.len();
+    divisors.reserve(n * (exponent - 1) as usize);
+    let mut multiple = base;
+    for _ in 1..=exponent {
+	for j in 0..n {
+	    divisors.push(divisors[j] * multiple);
+	}
+	multiple *= base;
+    }
+    divisors.sort();
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -16,5 +43,26 @@ mod tests {
     #[test]
     fn test_count_divisors() {
 	assert_eq!(count_divisors(28, &primes::get_primes()), 6);
+    }
+
+    #[test]
+    fn test_extend_divisors() {
+	let mut divisors = vec![1, 3, 9];
+	extend_divisors(&mut divisors, 2, 3);
+	assert_eq!(divisors, vec![1, 2, 3, 4, 6, 8, 9, 12, 18, 24, 36, 72]);
+    }
+
+    #[test]
+    fn test_get_proper_divisors() {
+	assert_eq!(get_proper_divisors(220, &primes::get_primes()),
+		   vec![1, 2, 4, 5, 10, 11, 20, 22, 44, 55, 110]);
+	assert_eq!(get_proper_divisors(284, &primes::get_primes()),
+		   vec![1, 2, 4, 71, 142]);
+    }
+
+    #[test]
+    fn test_sum_proper_divisors() {
+	assert_eq!(sum_proper_divisors(220), 284);
+	assert_eq!(sum_proper_divisors(284), 220);
     }
 }
