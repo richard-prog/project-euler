a7cbce2cdcce2d12846c135b34e89e60ce0dea3f
Richard Reitz <richard.reitz@protonmail.com>
2022-06-30 17:46:33 -0400

    Refactor problem 1 to be constant time

--- src/problems/p01.rs
+++ src/problems/p01.rs
@@ -1,13 +1,15 @@
 pub fn p01() -> u64 {
-    let relevant_multiple = |x| x % 3 == 0 || x % 5 == 0;
-    let mut sum = 0;
-    for n in 1..1000 {
-        if relevant_multiple(n) {
-            sum += n;
-        }
-    }
-    sum
+    let limit = 1000;
+    sum_positive_multiples(limit, 3)
+	+ sum_positive_multiples(limit, 5)
+	- sum_positive_multiples(limit, 15)
 }
+
+fn sum_positive_multiples(limit: u64, multiple: u64) -> u64 {
+    let scaled_limit = (limit - 1) / multiple;
+    scaled_limit * (scaled_limit + 1) / 2 * multiple
+}
+    
 
 #[cfg(test)]
 mod tests {
@@ -16,5 +18,20 @@ mod tests {
     #[test]
     fn check_solution() {
         assert_eq!(233168, p01());
+    }
+
+    #[test]
+    fn test_sum_positive_multiples() {
+	assert_eq!(sum_positive_multiples(10, 3), 18);
+	assert_eq!(sum_positive_multiples(9, 3), 9);
+	assert_eq!(sum_positive_multiples(1000, 3),
+		   (1..1000).map(|x| -> u64 {
+		       if x % 3 == 0 {
+			   return x;
+		       }
+		       0
+		   })
+		   .sum::<u64>()
+		   );
     }
 }
