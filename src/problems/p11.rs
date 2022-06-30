8754336d048dc05c75992272f02dfaf0537f0a1f
Richard Reitz <richard.reitz@protonmail.com>
2022-06-30 18:40:07 -0400

    Refactor problem 11

--- src/problems/p11.rs
+++ src/problems/p11.rs
@@ -1,44 +1,31 @@
-use std::fs;
+const NUM_ROWS: usize = 20;
+const NUM_COLS: usize = 20;
+
+use std::fs::File;
+use std::io::{BufRead, BufReader};
 
 pub fn p11() -> u64 {
-    let v = read_from_file();
+    let grid = read_from_file();
     
-    largest_product(&v, 4) as u64
+    largest_product(&grid, 4) as u64
 }
 
-fn read_from_file() -> Vec<Vec<u8>> {
-    let s = fs::read_to_string("p11.txt").unwrap();
-    let mut v = Vec::<Vec<u8>>::new();
-    let mut capacity = 0;
-    let mut num: u8 = 0;
-    let mut new_vec = Vec::with_capacity(capacity);
-    for &b in s.as_bytes() {
-	if b'0' <= b && b <= b'9' {
-	    num = num * 10 + b - b'0';
-	} else if b == b' ' {
-	    new_vec.push(num);
-	    capacity += 1;
-	    num = 0;
-	} else if b == b'\n' {
-	    new_vec.push(num);
-	    v.push(new_vec);
-	    new_vec = Vec::with_capacity(capacity + 1);
-	    capacity = 0;
-	    num = 0;
-	} else {
-	    panic!("File is only supposed to have digits, spaces, and newlines");
+fn read_from_file() -> [[u8; NUM_COLS]; NUM_ROWS] {
+    let lines = BufReader::new(File::open("p11.txt").unwrap()).lines();
+    let mut grid: [[u8; NUM_COLS]; NUM_ROWS] = [[0; NUM_COLS]; NUM_ROWS];
+    for (i, line) in lines.enumerate() {
+	for (j, num) in line.unwrap().split(' ').enumerate(){
+	    grid[i][j] = num.parse::<u8>().unwrap();
 	}
     }
-    v
+    grid
 }
     
-fn largest_product(v: &Vec<Vec<u8>>, l: usize) -> u32 {
-    let m = v.len();
-    let n = v[0].len();
+fn largest_product(grid: &[[u8; NUM_COLS]; NUM_ROWS], l: usize) -> u64 {
     let mut max_product = 0;
-    'outer: for row in v {
-	for &num in row {
-	    if num != 0 {
+    'outer: for i in 0..NUM_ROWS {
+	for j in 0..NUM_COLS {
+	    if grid[i][j] != 0 {
 		max_product = 1;
 		break 'outer;
 	    }
@@ -48,11 +35,11 @@ fn largest_product(v: &Vec<Vec<u8>>, l: usize) -> u32 {
 	return 0;
     }
     //horizontal
-    for i in 0..m {
-    	for j in 0..(n - l) {
+    for i in 0..NUM_ROWS {
+    	for j in 0..(NUM_COLS - l) {
     	    let mut cur_product = 1;
     	    for k in 0..l {
-    		cur_product *= v[i][j + k] as u32;
+    		cur_product *= grid[i][j + k] as u64;
     	    }
     	    if cur_product > max_product {
     		max_product = cur_product;
@@ -60,11 +47,11 @@ fn largest_product(v: &Vec<Vec<u8>>, l: usize) -> u32 {
     	}
     }
     //vertical
-    for j in 0..n {
-    	for i in 0..(m - l) {
+    for j in 0..NUM_COLS {
+    	for i in 0..(NUM_ROWS - l) {
     	    let mut cur_product = 1;
     	    for k in 0..l {
-    		cur_product *= v[i + k][j] as u32;
+    		cur_product *= grid[i + k][j] as u64;
     	    }
     	    if cur_product > max_product {
     		max_product = cur_product;
@@ -72,12 +59,12 @@ fn largest_product(v: &Vec<Vec<u8>>, l: usize) -> u32 {
     	}
     }
     //diagonal
-    for i in 0..(m - l) {
-	for j in 0..(n - l) {
+    for i in 0..(NUM_ROWS - l) {
+	for j in 0..(NUM_COLS - l) {
 	    //left-to-right diagonal
 	    let mut cur_product = 1;
 	    for k in 0..l {
-		cur_product *= v[i + k][j + k] as u32;
+		cur_product *= grid[i + k][j + k] as u64;
 	    }
 	    if cur_product > max_product {
 		max_product = cur_product;
@@ -85,7 +72,7 @@ fn largest_product(v: &Vec<Vec<u8>>, l: usize) -> u32 {
 	    //right-to-left diagonal
 	    cur_product = 1;
 	    for k in 0..l {
-		cur_product *= v[i + (l - 1) - k][j + k] as u32;
+		cur_product *= grid[i + (l - 1) - k][j + k] as u64;
 	    }
 	    if cur_product > max_product {
 		max_product = cur_product;
