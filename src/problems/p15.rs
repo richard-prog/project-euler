14d0cb245487b8b6af2607246cc87734ee89c712
Richard Reitz <richard.reitz@protonmail.com>
2022-06-30 23:09:39 -0400

    Optimize problem 15 (>7x speedup)

--- src/problems/p15.rs
+++ src/problems/p15.rs
@@ -1,22 +1,17 @@
 const GRID_SIZE: usize = 21;
 
 pub fn p15() -> u64 {
-    let mut grid: Vec<Vec<u64>> = Vec::with_capacity(GRID_SIZE);
-    grid.push(Vec::with_capacity(GRID_SIZE));
-    grid[0].push(0);
-    for _i in 1..GRID_SIZE {
-	grid[0].push(1);
-    }
-    for i in 1..GRID_SIZE {
-	grid.push(Vec::with_capacity(GRID_SIZE));
-	grid[i].push(1);
-	for j in 1..GRID_SIZE {
-	    let left = grid[i][j - 1];
-	    let up = grid[i - 1][j];
-	    grid[i].push(left + up);
-	}
+    let mut grid: [[u64; GRID_SIZE]; GRID_SIZE] = [[0; GRID_SIZE]; GRID_SIZE];
+    for j in 1..GRID_SIZE {
+    	grid[0][j] = 1;
     }
 
+    for i in 1..GRID_SIZE {
+    	grid[i][0] = 1;
+    	for j in 1..GRID_SIZE {
+    	    grid[i][j] = grid[i - 1][j] + grid[i][j - 1];
+    	}
+    }
     grid[GRID_SIZE - 1][GRID_SIZE - 1]
 }
 
