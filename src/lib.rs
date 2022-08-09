83e927b68215d1162d8d1fbae6a266740f69a5f5
Richard Reitz <richard.reitz@protonmail.com>
2022-08-09 19:37:13 -0400

    Enable receiving a solution of any type using enums

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 pub mod problems;
 
 pub mod divisors;
@@ -14,39 +16,55 @@ pub fn run_problems(problem_numbers: &[u16]) {
     }
 }
 
-fn run_problem(problem_number: u16, prime_vec: &Vec<u32>) -> i64 {
+fn run_problem(problem_number: u16, prime_vec: &Vec<u32>) -> Solution {
     match problem_number {
-        1 => problems::p01() as i64,
-        2 => problems::p02() as i64,
-        3 => problems::p03(prime_vec) as i64,
-        4 => problems::p04() as i64,
-        5 => problems::p05(prime_vec) as i64,
-        6 => problems::p06() as i64,
-        7 => problems::p07(prime_vec) as i64,
-        8 => problems::p08() as i64,
-        9 => problems::p09() as i64,
-        10 => problems::p10(prime_vec) as i64,
-        11 => problems::p11() as i64,
-        12 => problems::p12(prime_vec) as i64,
-        13 => problems::p13() as i64,
-        14 => problems::p14() as i64,
-        15 => problems::p15() as i64,
-        16 => problems::p16() as i64,
-        17 => problems::p17() as i64,
-        18 => problems::p18() as i64,
-        19 => problems::p19() as i64,
-        20 => problems::p20() as i64,
-        21 => problems::p21(prime_vec) as i64,
-        22 => problems::p22() as i64,
-        23 => problems::p23(prime_vec) as i64,
-        24 => problems::p24() as i64,
-        25 => problems::p25() as i64,
-        26 => problems::p26() as i64,
-        27 => problems::p27(prime_vec),
-        28 => problems::p28() as i64,
-        29 => problems::p29(prime_vec) as i64,
-        30 => problems::p30() as i64,
-        31 => problems::p31() as i64,
-        _ => 42,
+        1 => Solution::Unsigned(problems::p01()),
+        2 => Solution::Unsigned(problems::p02()),
+        3 => Solution::Unsigned(problems::p03(prime_vec)),
+        4 => Solution::Unsigned(problems::p04()),
+        5 => Solution::Unsigned(problems::p05(prime_vec)),
+        6 => Solution::Unsigned(problems::p06()),
+        7 => Solution::Unsigned(problems::p07(prime_vec)),
+        8 => Solution::Unsigned(problems::p08()),
+        9 => Solution::Unsigned(problems::p09()),
+        10 => Solution::Unsigned(problems::p10(prime_vec)),
+        11 => Solution::Unsigned(problems::p11()),
+        12 => Solution::Unsigned(problems::p12(prime_vec)),
+        13 => Solution::Unsigned(problems::p13()),
+        14 => Solution::Unsigned(problems::p14()),
+        15 => Solution::Unsigned(problems::p15()),
+        16 => Solution::Unsigned(problems::p16()),
+        17 => Solution::Unsigned(problems::p17()),
+        18 => Solution::Unsigned(problems::p18()),
+        19 => Solution::Unsigned(problems::p19()),
+        20 => Solution::Unsigned(problems::p20()),
+        21 => Solution::Unsigned(problems::p21(prime_vec)),
+        22 => Solution::Unsigned(problems::p22()),
+        23 => Solution::Unsigned(problems::p23(prime_vec)),
+        24 => Solution::Unsigned(problems::p24()),
+        25 => Solution::Unsigned(problems::p25()),
+        26 => Solution::Unsigned(problems::p26()),
+        27 => Solution::Signed(problems::p27(prime_vec)),
+        28 => Solution::Unsigned(problems::p28()),
+        29 => Solution::Unsigned(problems::p29(prime_vec)),
+        30 => Solution::Unsigned(problems::p30()),
+        31 => Solution::Unsigned(problems::p31()),
+        num => Solution::None{problem_number: num}
+    }
+}
+
+enum Solution {
+    Unsigned(u64),
+    Signed(i64),
+    None{problem_number: u16},
+}
+
+impl fmt::Display for Solution {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+	match self {
+	    Solution::Unsigned(x) => write!(f, "{}", x),
+	    Solution::Signed(x) => write!(f, "{}", x),
+	    Solution::None{problem_number} => write!(f, "Haven't solved problems number {}", problem_number),
+	}
     }
 }
