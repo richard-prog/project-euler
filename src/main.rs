88d81d7696d8eba0936091bf2f1e9f8d4ee1f87b
Richard Reitz <richard.reitz@protonmail.com>
2022-08-09 20:56:01 -0400

    Refactor main to take command line arguments. More work and error checking needed.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
+use std::env;
+
 fn main() {
-    // let problems = (1..=26).collect::<Vec<u16>>();
-    let problems = vec![2];
+    let problems = env::args()
+	.skip(1)
+	.map(|x| x.parse::<u16>().unwrap())
+	.collect::<Vec<u16>>();
     project_euler::run_problems(&problems);
 }
