dde7f8c4308bc550c5be434d5d38a21dafa7b130
Richard Reitz <richard.reitz@protonmail.com>
2022-08-30 13:38:54 -0400

    Remove unsafe

--- src/primes.rs
+++ src/primes.rs
@@ -1,5 +1,6 @@
 use std::fs::File;
-use std::io::Read;
+#[allow(unused_imports)]
+use std::io::{Read, Write};
 
 const MAX: usize = 2_000_000;
 
@@ -23,20 +24,26 @@ pub fn generate_primes() -> (Vec<bool>, Vec<u32>) {
             prime_vec.push(i as u32);
         }
     }
-    // let mut f = File::create("primes.txt").unwrap();
-    // f.write(as_u8_slice(&prime_vec)).unwrap();
+    // let mut f = File::create("primes.dump").unwrap();
+    // for prime in &prime_vec {
+    // 	f.write(&prime.to_le_bytes()).unwrap();
+    // }
     (sieve.to_vec(), prime_vec)
 }
 
 pub fn get_primes() -> Vec<u32> {
-    // let json_string = fs::read_to_string("primes.txt").unwrap();
-    // let prime_vec: Vec<u32> = serde_json::from_str(&json_string).unwrap();
-    let mut file = File::open("primes.txt").unwrap();
-    let mut buffer = Vec::new();
-    file.read_to_end(&mut buffer).unwrap();
-    let prime_vec = from_u8_vec(buffer);
-    for i in 0..100 {
-	println!("{}", prime_vec[i]);
+    let mut f = File::open("primes.dump").unwrap();
+    let mut bytes = Vec::new();
+    f.read_to_end(&mut bytes).unwrap();
+
+    let mut prime_vec = Vec::with_capacity(MAX / (MAX as f64).log(3.0) as usize);
+    let mut buffer = [0; 4];
+    let mut i = 0;
+    let l = bytes.len();
+    while i < l {
+	buffer[..4].copy_from_slice(&bytes[i..(i+4)]);
+	prime_vec.push(u32::from_le_bytes(buffer));
+	i += 4;
     }
     prime_vec
 }
@@ -59,34 +66,6 @@ pub fn factor(mut num: u64, primes: &Vec<u32>) -> Vec<(u32, u32)> {
     }
     ret
 }
-
-// fn as_u8_slice(v: &[u32]) -> &[u8] {
-//     unsafe {
-// 	let u8_ptr = v.as_ptr() as *const u8;
-// 	let num_bytes = v.len() * std::mem::size_of::<u32>();
-// 	std::slice::from_raw_parts(u8_ptr, num_bytes)
-//     }
-// }
-
-fn from_u8_vec(v: Vec<u8>) -> Vec<u32> {
-    let ptr = v.as_ptr();
-    let length = v.len();
-    let capacity = v.capacity();
-    let element_size = std::mem::size_of::<u32>();
-
-    assert_eq!(capacity % element_size, 0);
-
-    unsafe {
-	std::mem::forget(v);
-
-	Vec::from_raw_parts(
-	    ptr as *mut u32,
-	    length / element_size,
-	    capacity / element_size
-	)
-    }
-}
-
 
 #[cfg(test)]
 mod tests {
